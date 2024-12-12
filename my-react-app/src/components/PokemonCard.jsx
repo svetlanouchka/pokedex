c5c4fad2d6cddda844b9f8db2f3e48186420efa9
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2024-12-12 16:10:01 +0100

    added dynamic pokemon

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,13 +1,27 @@
 function PokemonCard () {
+    const pokemon = pokemonList[1];
+    
 return (
     <>
     <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image of Bulbausar"/>
+    <img src={pokemon.imgSrc}/>
     </figure>
-    <figcaption>Bulbausar</figcaption>
+    <figcaption>{pokemon.name}</figcaption> ? <p>???</p>
     </>
 );
 
 }
+
+const pokemonList = [
+    {
+    name: "bulbasaur",
+    imgSrc:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+    name: "mew",
+    },
+];
+
 
 export default PokemonCard;
