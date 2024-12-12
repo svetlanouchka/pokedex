f0f8d05190691e9dd8b160590ea20cda2998dbf7
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2024-12-12 15:48:09 +0100

    added pokemancard

---
+++ my-react-app/src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+function PokemonCard () {
+return (
+    <>
+    <figure>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image of Bulbausar"/>
+    </figure>
+    <figcaption>Bulbausar</figcaption>
+    </>
+);
+
+}
+
+export default PokemonCard;
