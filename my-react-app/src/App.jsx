08a28119306f22d8c4a01b9441d77c03f360a6ee
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2024-12-13 13:27:21 +0100

    added useeffect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,6 +6,12 @@ import PokemonCard from './components/PokemonCard'
 import NavBar from './components/NavBar'
 
 function App() {
+  useEffect(
+    () => {
+    alert('hello pokemon trainer :)')
+    }
+  , []
+  )
 const [pokemonIndex, setPokemonIndex] = useState(0);
 const pokemonList = [
     {
--- my-react-app/src/components/NavBar.jsx
+++ my-react-app/src/components/NavBar.jsx
@@ -1,18 +1,26 @@
-const NavBar = ({pokemonList, setPokemonIndex}) => {
-    return (
-    <nav>
-        {pokemonList.map((pokemon, index) => (
-        <button
-            key={pokemon.name}
-            type="button"
-            onClick={() => setPokemonIndex(index)}
-        >
-            {pokemon.name}
-        </button>
-        ))};
-    </nav>
-    );
-}
+const NavBar = ({ pokemonList, setPokemonIndex }) => {
+    const handlePikachuClick = () => {
+        alert('pika pikachu');
+    };
 
+    return (
+        <nav>
+            {pokemonList.map((pokemon, index) => (
+                <button
+                    key={pokemon.name}
+                    type="button"
+                    onClick={() => {
+                        setPokemonIndex(index);
+                        if (pokemon.name === "pikachu") {
+                            handlePikachuClick();
+                        }
+                    }}
+                >
+                    {pokemon.name}
+                </button>
+            ))}
+        </nav>
+    );
+};
 
 export default NavBar;
